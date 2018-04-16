66de525f2af633827d32d86293b224289cbfc2a1
Cui Yan <yanx.cui@intel.com>
2018-04-16 16:37:10 +0800

    Add main file
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

---
+++ test/test-main.js
@@ -0,0 +1,30 @@
+console.log("Test APIs for Grasping web information...");
+
+const csv = require("../modules/savingcsvfile").SavingCSVFile;
+const Web = require("../modules/graspingwebinfo").GraspingWebInfo;
+
+(async function() {
+    await csv.Open("ubuntu")
+        .then(function() {
+        console.log("Open csv file success!");
+    })
+        .catch(function() {
+        console.log("Open csv file fail!");
+    });
+
+    let driver = "ubuntu";
+    //let driver = "andriod";
+    let RemoteURL = "http://127.0.0.1:8080/test/";
+    //let RemoteURL = "http://brucedai.github.io/nt/test/index-local.html";
+    let WaitTime = 10000;
+    await Web.init(driver, csv, RemoteURL, WaitTime);
+    await Web.Grasp();
+
+    await csv.Close()
+        .then(function() {
+        console.log("Close csv file success!");
+    })
+        .catch(function() {
+        console.log("Close csv file fail!");
+    });
+})().then(_ => console.log("Test complete!"), err => console.log("ERROR: " + err));
