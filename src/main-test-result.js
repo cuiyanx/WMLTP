893f2e0e7d3a151c9626b48ebc25f1fb7cb44414
Cui Yan <yanx.cui@intel.com>
2018-04-19 15:56:30 +0800

    Separate chrome driver out
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

--- src/main-test-result.js
+++ src/main-test-result.js
@@ -1,39 +1,33 @@
 console.log("Grasping test result...");
 
+const chromedriver = require("../lib/chrome-driver").ChromeDriver;
 const csv = require("../lib/saving-csv-file").SavingCSVFile;
-const Web = require("../lib/grasping-web-info").GraspingWebInfo;
+const gts = require("../lib/grasping-test-result").TestResult;
 
 (async function() {
     await csv.setPlatform("ubuntu");
     await csv.Open()
         .then(function() {
         console.log("Open csv file success!");
-    })
-        .catch(function() {
-        console.log("Open csv file fail!");
     });
 
-    await Web.setChromePath("/usr/bin/chromium-browser-unstable");
-    await Web.setCSVFile(csv);
-    await Web.setPlatforms("ubuntu");
-    await Web.setWebMLSwitch(false);
-    await Web.CreateDriver();
-    await Web.Open("http://brucedai.github.io/nt/test/index-local.html");
-    await Web.GraspTestResult()
+    await chromedriver.setChromePath("/usr/bin/chromium-browser-unstable");
+    await chromedriver.setPlatform("ubuntu");
+    await chromedriver.setWebMLSwitch(false);
+    await chromedriver.create();
+    await chromedriver.open("http://brucedai.github.io/nt/test/index-local.html");
+    await chromedriver.wait(10000);
+
+    let driver = chromedriver.getDriver();
+    let By = chromedriver.getBy();
+    await gts.grasp(driver, By, csv)
         .then(function() {
         console.log("Grasp test result success!");
-    })
-        .catch(function(e) {
-        console.log("Grasp test result fail!");
-        throw e;
     });
-    await Web.Close();
 
+    await chromedriver.close();
     await csv.Close()
         .then(function() {
         console.log("Close csv file success!");
-    })
-        .catch(function() {
-        console.log("Close csv file fail!");
     });
 })().then(_ => console.log("Grasping test result is completed!"), err => console.log("ERROR: " + err));
