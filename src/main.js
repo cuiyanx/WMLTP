a78e8e8debfaab92b4142777992203927e4784f7
Cui Yan <yanx.cui@intel.com>
2018-05-10 13:16:03 +0800

    Add exiting process after catching error
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ const childProcess = require("child_process");
 
 (async function() {
     await childProcess.spawnSync("node", ["./examples/download-newest-package.js"], {stdio: "inherit"});
-    await childProcess.spawnSync("node", ["./examples/install-newest-package.js"], {stdio: "inherit"});
+
+    let processInstall = await childProcess.spawnSync("node", ["./examples/install-newest-package.js"], {stdio: [process.stdin, process.stdout, "pipe"]});
+
+    if (processInstall.stderr.toString() != "") {
+        process.exit(1);
+    }
+
     await childProcess.spawnSync("node", ["./examples/grasp-test-result.js"], {stdio: "inherit"});
 })().then(function() {
     console.log("Test completed!");
